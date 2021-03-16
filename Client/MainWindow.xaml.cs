c96069496dd809228b25f6b81dfbc07fe4a73d1e
bjoonly <elumnaya@gmail.com>
2021-03-17 01:07:20 +0200

    Init

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Xml.Serialization;
+
+namespace Client
+{
+
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = null;
+        int interval = 15;
+        TcpClient tcpClient = null;
+        IPEndPoint iPAddress =new IPEndPoint(IPAddress.Parse("127.0.0.1"),7777);
+
+        int i = 15;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0,0,0,0,1000);
+         
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            i--;
+            if (i == 0)
+            {
+                Task.Run(() => GetScreen());
+                i = interval;
+            }
+            
+            
+        }
+        private void GetScreen()
+        {
+           
+            try
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect(iPAddress);
+
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(FileTransferInfo));
+                    var info = (FileTransferInfo)serializer.Deserialize(tcpClient.GetStream());
+                    using (FileStream fs = new FileStream(info.Name, FileMode.Create, FileAccess.Write))
+                    {
+                        fs.Write(info.Data, 0, info.Data.Length);
+                    }
+                    System.Windows.Application.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+
+                        listBox.Items.Add(info);
+
+                    }));
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                
+                tcpClient.Close();
+            }
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (ComboBoxItem)((ComboBox)sender).SelectedItem;
+            if (int.TryParse(item.Tag.ToString(), out int sec))
+            {
+              
+                interval =  sec;
+                i = sec;
+       
+            }          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => GetScreen()).ContinueWith((result)=>timer.Start());
+         
+            
+           
+        }
+    }
+    [Serializable] 
+    public class FileTransferInfo
+    {
+     
+        public string Name { get; set; }
+      
+        public byte[] Data { get; set; }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    [Serializable] 
+    public class FileTransferInfo
+    {     
+        public string Name { get; set; }     
+        public byte[] Data { get; set; }
+    }
+    class Program
+    {
+        static int count = 1;
+        static IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+        static int port = 7777;
+        static void Main(string[] args)
+        {
+         
+            IPEndPoint localEndPoint = new IPEndPoint(iPAddress, port);           
+            TcpListener server = new TcpListener(localEndPoint);                              
+            try
+            {
+                server.Start(10);
+
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connect from "+client.Client.RemoteEndPoint+" at " +DateTime.Now);
+                    Task.Run(() => Work(client));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }        
+        }
+        static void Work(TcpClient client) 
+        {
+            try
+            {
+                Console.WriteLine("Sending...");
+                string path = $"Screenshot{count}.png";
+                Graphics graph = null;
+
+                var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height);
+                graph = Graphics.FromImage(bmp);
+                graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                bmp.Save(path);
+                FileTransferInfo info = new FileTransferInfo();
+                info.Name = Path.GetFileName(path);
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+
+                    byte[] fileData = new byte[fs.Length];
+                    fs.Read(fileData, 0, fileData.Length);
+                    info.Data = fileData;
+                }
+
+                XmlSerializer serializer = new XmlSerializer(typeof(FileTransferInfo));
+                using (NetworkStream stream = client.GetStream())
+                {
+                    serializer.Serialize(stream, info);
+                }
+                Console.WriteLine("Screenshot send at "+DateTime.Now+"\n"+new string('-',100)+"\n\n");
+                File.Delete(path);
+                count++;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+              
+                client.Close();
+            }
+        }
+        
+    }
+}
